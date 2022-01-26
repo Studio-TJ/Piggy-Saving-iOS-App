745d7143ca253dcd7916c8453ac7cdbb6e1a1012
Tianyu Liu <liu.tianyu93@hotmail.com>
2022-01-26 16:54:10 +0100

    Initial Commit

---
+++ PiggySaving/PiggySavingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggySavingApp.swift
+//  PiggySaving
+//
+//  Created by Tianyu Liu on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PiggySavingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
