06b775d2a2f72bf5df512eff38bb2b5051f66b17
Tianyu Liu <liu.tianyu93@hotmail.com>
2022-02-18 20:55:05 +0100

    Modify left swipe edit cost behavior

--- PiggySaving/MainView/SavingListViews/CostListItemView.swift
+++ PiggySaving/MainView/SavingListViews/CostListItemView.swift
@@ -16,50 +16,61 @@ struct CostListItemView: View {
     @EnvironmentObject var states: States
     
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text(cost.dateLocalizedMonthDay)
-                    .font(Fonts.CAPTION)
-                    .foregroundColor(Color("Grey"))
-                HStack {
-                    Text(cost.description ?? "")
-                        .font(Fonts.BODY_CHINESE_NORMAL)
-                    Spacer(minLength: 30)
-                }
-                .fixedSize(horizontal: false, vertical: true)
+        ZStack {
+            HStack {
+                Spacer()
+                Image(systemName: "square.and.pencil")
+                    .padding(.trailing, 10)
+                    .opacity(min(abs(offset.width) / Double(70), 1))
             }
-            Spacer()
-            Text("-" + CURRENCY_SYMBOL + String(format: "%.2f", -cost.amount))
-                .font(.system(size: 16).monospacedDigit())
-                .foregroundColor(Color("Grey"))
-        }
-        .offset(x: offset.width, y: 0)
-        .gesture(
-            DragGesture()
-                .onChanged { gesture in
-                    if gesture.translation.width < 0 {
-                        offset = gesture.translation
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(cost.dateLocalizedMonthDay)
+                        .font(Fonts.CAPTION)
+                        .foregroundColor(Color("Grey"))
+                    HStack {
+                        Text(cost.description ?? "")
+                            .font(Fonts.BODY_CHINESE_NORMAL)
+                        Spacer(minLength: 30)
                     }
+                    .fixedSize(horizontal: false, vertical: true)
                 }
-                .onEnded { gesture in
-                    if gesture.translation.width < -100 {
-                        UINotificationFeedbackGenerator().notificationOccurred(.success)
-                        popupHandler.view = AnyView(CostEditView(edit: true,
-                                                                 sequence: cost.sequence,
-                                                                 date: cost.dateFormatted,
-                                                                 amount: -cost.amount,
-                                                                 description: cost.description ?? "")
-                                                        .environmentObject(popupHandler)
-                                                        .environmentObject(states))
-                        withAnimation(.linear(duration: 1)) {
-                            popupHandler.popuped = true
+                Spacer()
+                Text("-" + CURRENCY_SYMBOL + String(format: "%.2f", -cost.amount))
+                    .font(.system(size: 16).monospacedDigit())
+                    .foregroundColor(Color("Grey"))
+            }
+            .offset(x: offset.width, y: 0)
+            .gesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        if gesture.translation.width < 0 {
+                            offset = gesture.translation
+                        }
+                        if gesture.translation.width < -70 {
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                        }
+                        print(offset.width)
+                    }
+                    .onEnded { gesture in
+                        if gesture.translation.width < -65 {
+                            popupHandler.view = AnyView(CostEditView(edit: true,
+                                                                     sequence: cost.sequence,
+                                                                     date: cost.dateFormatted,
+                                                                     amount: -cost.amount,
+                                                                     description: cost.description ?? "")
+                                                            .environmentObject(popupHandler)
+                                                            .environmentObject(states))
+                            withAnimation(.linear(duration: 1)) {
+                                popupHandler.popuped = true
+                            }
+                        }
+                        withAnimation(.spring()) {
+                            offset = .zero
                         }
                     }
-                    withAnimation(.spring()) {
-                        offset = .zero
-                    }
-                }
-        )
+            )
+        }
     }
 }
 
